7424e5c857a3f72d14a19acd17faab4e93045502
Vinay Yadav <vinayyadav91190@gmail.com>
2023-03-03 19:34:35 +0530

    implement default method precedence hierarchy

---
+++ src/defaultmethod/DefaultStarter.java
@@ -0,0 +1,39 @@
+package defaultmethod;
+// Rule 1: Classes always win. A method defined in the class or its superclass takes 
+
+//precedence over the default method definition that is available in the interface.
+
+// Rule 2: Otherwise, sub-interfaces win: the method with the exact signature
+// in the most specific default-functionality providing interface will be selected.
+
+// Rule 3: If the choice continues to be ambiguous, the class that inherits multiple
+// interfaces should explicitly select the default method implementation to be used just
+// by overriding it and the overridden method should have an explicit call to the desired default behavior.
+
+//interface
+interface Greeting {
+    default void hello() {
+        System.out.println(" hello from A");
+    }
+}
+
+// interface
+// interface can extends one interface
+interface GreetingExtn extends Greeting {
+    default void hello() {
+        System.out.println(" hello from Greeting");
+    }
+}
+
+class Greet { // comment and uncomment this class to try more possibilities
+    public void hello() {
+        System.out.println("hello from Greet");
+    }
+}
+
+// class can implements the multiple interface
+public class DefaultStarter extends Greet implements Greeting, GreetingExtn {
+    public static void main(String[] args) {
+        new DefaultStarter().hello();
+    }
+}
