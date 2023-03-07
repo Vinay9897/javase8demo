b8ac5fd168d23fcc76a470c167c7078a17731b19
Vinay Yadav <vinayyadav91190@gmail.com>
2023-03-07 18:38:42 +0530

    create the main class EventManager.java

--- src/hotelbookingmanagement/Guest.java
+++ src/hotelbookingmanagement/Guest.java
@@ -1,5 +1,17 @@
 package hotelbookingmanagement;
 
-public class Guest {
+import java.sql.Date;
 
+class Guest {
+    String name;
+    Date date;
+    String country;
+    String language;
+    String hobby;
+}
+
+public class EventManager {
+    public static void main(String[] args) {
+
+    }
 }
