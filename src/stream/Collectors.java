ab5ba5cc9836c5997662bd5a5e75cc62fdcd6d16
Vinay Yadav <vinayyadav91190@gmail.com>
2023-03-07 18:40:58 +0530

    Guest.java rename with EventManager.java

--- src/stream/Collectors.java
+++ src/stream/Collectors.java
@@ -1,8 +1,9 @@
 package stream;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.ArrayList;
-import java.util.stream.;;
+import java.util.stream.*;;
 // Collectors which provides implementations of the Collector interface through 
 // many useful static methods like toList(), toMap(), groupingBy(), maxBy(), minBy() etc.
 
@@ -13,16 +14,30 @@ public class Collectors {
     // <R, A> R collect(Collector<? super T, A, R> collector);
     public static void main(String[] args) {
         List<Employee> lstEmp = Employee.getEmpList();
-        //displaying the sample data
+        // displaying the sample data
         System.out.println("*****Sample Data*****");
         lstEmp.forEach((e) -> System.out.println(e.getId() + ":" + e.getName() + ":" + e.getSal()));
-        //displaying the data after applying filter and increasing salary
+        // displaying the data after applying filter and increasing salary
         System.out.println("*****Data after applying filter and increasing salary*****");
-        
-        // List<Employee> lstNewEmp = lstEmp.stream().filter(emp -> emp.getYearsInOrg() < 1)
-        //     .map(e -> { e.setSal(e.getSal() + 5000); return e; })
-        //     .collect(Collectors.toList());
-        // lstNewEmp.forEach((e) -> System.out.println(e.getId() + ":" + e.getName() + ":" + e.getSal())); 
+
+        // List<Employee> lstNewEmp = lstEmp.stream().filter(emp -> emp.getYearsInOrg()
+        // < 1)
+        // .map(e -> { e.setSal(e.getSal() + 5000); return e; })
+        // .collect(Collectors.toList());
+        // lstNewEmp.forEach((e) -> System.out.println(e.getId() + ":" + e.getName() +
+        // ":" + e.getSal()));
+
+        // Optional<Integer> max = lstEmp.stream().map(e ->
+        // e.getSal()).reduce(Integer::max); // Finding the maximum
+        // // salalry
+        // List<Employee> lstMaxEmp = lstEmp.stream()
+        // .filter(e -> e.getSal().equals(max.get())) // Finding employees with the
+        // maximum salary
+        // .collect(Collectors.toList());
+        // System.out.println("\nEmployees having maximum salary after increment:");
+        // lstMaxEmp.forEach(e -> System.out.println(e.getId() + ":" + e.getName() + ":"
+        // + e.getSal()));
+
     }
 }
 
