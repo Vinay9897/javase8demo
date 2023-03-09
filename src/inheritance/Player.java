62c60d30f7d7e95d2d9f666e5db1e46962ab73a1
Vinay Yadav <vinayyadav91190@gmail.com>
2023-03-09 21:09:31 +0530

    intialize the member variable of PlayerRating.java

---
+++ src/inheritance/Player.java
@@ -0,0 +1,20 @@
+package inheritance;
+
+class PlayerRating {
+    int playerPosition;
+    String playerName;
+    float criticOneRating;
+    float criticTwoRating;
+    float criticThreeRating;
+    float averageRating;
+    char category;
+
+    // construtor
+
+}
+
+public class Player {
+    public static void main(String[] args) {
+
+    }
+}
