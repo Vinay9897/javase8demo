174721a9dbc25ed2ceb6ab56a1a6782f5800b1e0
Vinay Yadav <vinayyadav91190@gmail.com>
2023-03-10 16:22:34 +0530

    count the no of loanCounter using static

---
+++ src/staticdemo/Loan.java
@@ -0,0 +1,55 @@
+package staticdemo;
+
+public class Loan {
+    // To count the number of objects created using static variable and method
+    private static int loanCounter;
+    int loanNo;
+    int accountNo;
+    int loanAmount;
+
+    Loan() {
+        loanCounter++;
+    }
+
+    Loan(int loanNo, int accountNo, int loanAmount) {
+        this.loanNo = loanNo;
+        this.accountNo = accountNo;
+        this.loanAmount = loanAmount;
+        loanCounter++;
+    }
+
+    public int getAccountNo() {
+        return accountNo;
+    }
+
+    public void setAccountNo(int accountNo) {
+        this.accountNo = accountNo;
+    }
+
+    public int getLoanAmount() {
+        return loanAmount;
+    }
+
+    public void setLoanAmount(int loanAmount) {
+        this.loanAmount = loanAmount;
+    }
+
+    public int getLoanNo() {
+        return loanNo;
+    }
+
+    public void setLoanNo(int loanNo) {
+        this.loanNo = loanNo;
+    }
+
+    public static int getLoanCounter() {
+        return loanCounter;
+    }
+
+    public static void main(String[] args) {
+        Loan loan1 = new Loan();
+        Loan loan2 = new Loan(132, 3842, 300);
+        System.out.println(loan1.getLoanCounter());
+
+    }
+}
