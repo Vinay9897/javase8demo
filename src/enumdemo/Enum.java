bd877d62a339332d5381ca675b71eae45dd7ee0a
Vinay Yadav <vinayyadav91190@gmail.com>
2023-03-10 18:54:37 +0530

    display output using reference of Bank class

--- src/enumdemo/Enum.java
+++ src/enumdemo/Enum.java
@@ -3,3 +3,30 @@ package enumdemo;
 enum Designation {
     CEO, GeneralManager, RegionalManager, BranchManager
 }
+
+class Bank {
+    public void roleDefined(Designation designation) {
+        switch (designation) {
+            case CEO:
+                System.out.println("Designation is CEO");
+                break;
+            case GeneralManager:
+                System.out.println("Designation is GeneralManager");
+                break;
+            case RegionalManager:
+                System.out.println("Designation is RegionalManager");
+                break;
+            case BranchManager:
+                System.out.println("Desiganation is BranchManager");
+                break;
+        }
+    }
+}
+
+public class Enum {
+    public static void main(String[] args) {
+        Bank bank = new Bank();
+        bank.roleDefined(Designation.BranchManager);
+        bank.roleDefined(Designation.RegionalManager);
+    }
+}
