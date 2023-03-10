20abfbeaf722841e7c7a1fbc3b8cd5668385530d
Vinay Yadav <vinayyadav91190@gmail.com>
2023-03-10 14:22:05 +0530

    define the SingleEventRegistration.java class

--- src/polymorphismdemo/SingleEventRegistration.java
+++ src/polymorphismdemo/SingleEventRegistration.java
@@ -1,5 +1,26 @@
 package polymorphismdemo;
 
-public class SingleEventRegistration {
+public class SingleEventRegistration extends EventRegistration {
+    int participantNo;
 
+    public SingleEventRegistration(String name, String nameOfEvent, int participantNo) {
+        super(name, nameOfEvent);
+        this.name = name;
+        this.nameOfEvent = nameOfEvent;
+        this.participantNo = participantNo;
+    }
+
+    public int getParticipantNo() {
+        return participantNo;
+    }
+
+    public void setParticipantNo(int participantNo) {
+        this.participantNo = participantNo;
+    }
+
+    @Override
+    public void registerEvent() {
+        // TODO Auto-generated method stub
+        super.registerEvent();
+    }
 }
